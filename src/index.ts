b60e2fa596e6296bf652f6f484468e7ac269e65f
Paulo Margarido <paulo.margarido@shopify.com>
2021-01-15 10:37:03 -0500

    Use the Shopify Library OAuth code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import shopifyAuth from './auth';
+import Shopify from '@shopify/shopify-api';
 
 export default shopifyAuth;
 
 export * from './auth';
+export * from '@shopify/shopify-api';
 
+export { Shopify };
 export {default as verifyRequest} from './verify-request';
